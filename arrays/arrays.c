f5098bd6dcfe926a73d6288de437a4035c9e2f5a
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2019-06-04 11:33:44 -0500

    Cleaned up code. Removed some commented out code as well as some print statements.

--- arrays/arrays.c
+++ arrays/arrays.c
@@ -50,18 +50,13 @@ void destroy_array(Array *arr) {
 void resize_array(Array *arr) {
 
   // Create a new element storage with double capacity
-  // char **newStorage = malloc(20);
   int newCapcity = arr->capacity * 2;
   char **newStorage = malloc( newCapcity * sizeof(char *) );
-  // newStorage[0] = arr->elements[0];
-  // printf("%s\n", newStorage[0]);
 
   // Copy elements into the new storage
   for (int i = 0; i < arr->count; i++){
     newStorage[i] = arr->elements[i];
   }
-  // printf("new string: %s\n", newStorage[0]);
-  // printf("new array size: %ld\n", sizeof(*newStorage));
   // Free the old elements array (but NOT the strings they point to)
   free(arr->elements);
   // Update the elements and capacity to new values
@@ -115,8 +110,8 @@ void arr_insert(Array *arr, char *element, int index) {
   for (int i = arr->count-1; i >= index; i--){
     arr->elements[i+1] = arr->elements[i];
   }
+
   // Copy the element (hint: use `strdup()`) and add it to the array
-  // char *temp = strdup(element);
   arr->elements[index] = strdup(element);
   // Increment count by 1
   arr->count++;
@@ -128,14 +123,12 @@ void arr_insert(Array *arr, char *element, int index) {
 void arr_append(Array *arr, char *element) {
   // Resize the array if the number of elements is over capacity
   // or throw an error if resize isn't implemented yet.
-  if (arr->count > (arr->capacity - 1)){
-    // fprintf(stderr, "array at capacity\n");
-    // exit(1);
+  if ((arr->count + 1) > arr->capacity){
+    printf("going over capcity need to resize array\n");
     resize_array(arr);
   }
-  // Copy the element and add it to the end of the array
-  // char *temp = strdup(element);
 
+  // Copy the element and add it to the end of the array
   arr->elements[arr->count] = strdup(element);
   // Increment count by 1
   arr->count++;
@@ -166,14 +159,13 @@ void arr_remove(Array *arr, char *element) {
   // after finding the element free that spot in memory
   free(arr->elements[found]);
   
-  // printf("String: %s, Index: %d\n", element, found);
   // Shift over every element after the removed element to the left one position
   for (int i = found; i < arr->count-1; i++){
     arr->elements[i] = arr->elements[i+1];
   }
   // Decrement count by 1
-  printf("first spot: %s\n", arr_read(arr, 0));
-  printf("last spot: %s\n", arr->elements[arr->count-1]);
+  // printf("first spot: %s\n", arr_read(arr, 0));
+  // printf("last spot: %s\n", arr->elements[arr->count-1]);
   arr->count--;
 }
 
